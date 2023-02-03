16331f4c94256858f113f83ade3f591c47d3911e
Ronaldeived Silva <ronaldeivedsilva@MacBook.local>
2023-02-03 19:06:04 -0300

    Adding the files from local repository

---
+++ Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using devio;
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ConsumerConfig { BootstrapServers = "localhost:9092", GroupId = "devio" };
+
+using var consumer = new ConsumerBuilder<string, Course>(config)
+    .SetValueDeserializer(new AvroDeserializer<Course>(schemaRegistry).AsSyncOverAsync())
+    .Build();
+
+consumer.Subscribe("courses");
+
+while (true)
+{
+    var result = consumer.Consume();
+    Console.WriteLine($"Message: {result.Message.Key} - {DateTime.Now}");
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using devio;
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<string, Course>(config)
+    .SetValueSerializer(new AvroSerializer<Course>(schemaRegistry))
+    .Build();
+
+var message = new Message<string, Course>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = new Course
+    {
+        Id = Guid.NewGuid().ToString(),
+        Description = $"Course of Apache Kafka {Guid.NewGuid().ToString()}"
+    }
+};
+
+var result = await producer.ProduceAsync("courses", message);
+
+Console.WriteLine(result.Offset);
